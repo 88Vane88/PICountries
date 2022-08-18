eea1d6d550dfeff3c0c0d4de5e4c4c574cb33ea1
88vane88 <vancig88@gmail.com>
2022-08-18 13:05:57 +0200

    store.index

---
+++ client/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "../reducer";
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+  )
+);
+
+export default store;
